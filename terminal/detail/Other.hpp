eaafd80a031f0550274fb7cfa97bc2c0dda3912b
Ethan MacDonald <ethanmacdonald04@gmail.com>
2016-12-03 19:03:19 -0400

    did a ton of cleanups, added a ton including modal, circle, and diagonal line algorithims.

--- terminal/detail/Other.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-std::vector<std::vector<char> > screen_space;
-std::vector<char> temp_screen_space;
-
-std::vector<std::vector<char> > game_space;
-std::vector<char> temp_game_space;
